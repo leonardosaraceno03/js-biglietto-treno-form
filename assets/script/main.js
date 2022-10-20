af8d8b3195b19426c604076121433099919c90f1
LeonardoSaraceno <leonardosaraceno03@gmail.com>
2022-10-20 17:39:18 +0200

    Da sistemare lo stile

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,27 +9,40 @@ function generaBiglietto(){
 
     if(UserAge < 18){
         let Price= ((UserKm * 0.21)*0.8).toFixed(2);
-        document.getElementById('info').innerHTML = `il prezzo dato che sei minorenne corrisponde a: ${Price} € `
-        document.getElementById('PassengerName').innerHTML = `Nome Passeggero : ${UserName}`
-        document.getElementById('NumeroCarrozza').innerHTML = `Carrozza : ${NCarrozza}`
-        document.getElementById('inputCPCode').innerHTML = `Codice CP : ${CPCode}`
+        document.getElementById('info').innerHTML = `${Price} € `
+        document.getElementById('PassengerName').innerHTML = `${UserName}`
+        document.getElementById('NumeroCarrozza').innerHTML = `${NCarrozza}`
+        document.getElementById('inputCPCode').innerHTML = `${CPCode}`
         document.getElementById('offer').innerHTML = `Biglietto ridotto`
     }
     else if(UserAge > 65){
         let Price=((UserKm * 0.21) *0.6).toFixed(2);
-        document.getElementById('info').innerHTML = `il prezzo dato che sei vecchio corrisponde a: ${Price} `
-        document.getElementById('PassengerName').innerHTML = `Nome Passeggero : ${UserName}`
-        document.getElementById('NumeroCarrozza').innerHTML = `Carrozza : ${NCarrozza}`
-        document.getElementById('inputCPCode').innerHTML = `Codice CP : ${CPCode}`
+        document.getElementById('info').innerHTML = `${Price} `
+        document.getElementById('PassengerName').innerHTML = `${UserName}`
+        document.getElementById('NumeroCarrozza').innerHTML = `${NCarrozza}`
+        document.getElementById('inputCPCode').innerHTML = `${CPCode}`
         document.getElementById('offer').innerHTML = `Biglietto over 65`
     }
     else{
         let Price= (UserKm * 0.21).toFixed(2);
-        document.getElementById('info').innerHTML = `non ti meriti nessuno sconto, paga : ${Price} `
-        document.getElementById('PassengerName').innerHTML = `Nome Passeggero : ${UserName}`
-        document.getElementById('NumeroCarrozza').innerHTML = `Carrozza : ${NCarrozza}`
-        document.getElementById('inputCPCode').innerHTML = `Codice CP : ${CPCode}`
+        document.getElementById('info').innerHTML = `${Price} `
+        document.getElementById('PassengerName').innerHTML = `${UserName}`
+        document.getElementById('NumeroCarrozza').innerHTML = `${NCarrozza}`
+        document.getElementById('inputCPCode').innerHTML = ` ${CPCode}`
         document.getElementById('offer').innerHTML = `Biglietto standard`
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
